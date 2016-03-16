13a2b97693413e74694e2c77bf212b5ef3a639fc
Michael Kuehn <mkuehn10@gmail.com>
2016-03-16 14:44:25 -0400

    Complete svm mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C = 10000)
+t0 = time()
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+clf.fit(features_train, labels_train)
+print "training time:",round(time()-t0,3),"s"
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:",round(time()-t0,3),"s"
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
 
+print pred[10]
+print pred[26]
+print pred[50]
+
+print sum(pred)
 #########################################################
 
 
