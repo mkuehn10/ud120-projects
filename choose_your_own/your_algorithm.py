ca49c28eda35fa906399e41b8aec6793d309c56a
Michael Kuehn <mkuehn10@gmail.com>
2016-03-17 10:24:51 -0400

    Complete cyoa mini-project

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
-
+from time import time
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
@@ -30,10 +30,20 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+print "before creating clf"
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors = 75, weights = 'distance', metric = 'manhattan')
+print "created classifier, fitting..."
+t0 = time()
+clf.fit(features_train, labels_train)
 
-
-
-
+print "training time:",round(time()-t0,3),"s"
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:",round(time()-t0,3),"s"
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
 
 
 
