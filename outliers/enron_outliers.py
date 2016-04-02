eab1268c723d00543ac00b2ab59176f8ecf8f1a3
Michael Kuehn <mkuehn10@gmail.com>
2016-04-02 18:05:54 -0400

    Complete outliers mini-project

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -3,6 +3,7 @@
 import pickle
 import sys
 import matplotlib.pyplot
+import pprint
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -10,10 +11,28 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop('TOTAL', 0)
+#pprint.pprint(data_dict)
+for key in data_dict.keys():
+    #pprint.pprint(key)
+    bonus = data_dict[key]['bonus']
+    salary = data_dict[key]['salary']
+    if (( bonus > 5000000) and (salary > 1000000) and (bonus != "NaN") and (salary != "NaN")):
+        
+        print key
+        print data_dict[key]['bonus']
+        print data_dict[key]['salary']
+
+
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 
-
+import pprint
 def outlierCleaner(predictions, ages, net_worths):
-    """
+	"""
         Clean away the 10% of points that have the largest
         residual errors (difference between the prediction
         and the actual net worth).
 
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
-    """
-    
-    cleaned_data = []
-
-    ### your code goes here
-
-    
-    return cleaned_data
-
+	"""
+	
+	cleaned_data = []
+	error = 0
+	for pred, age, net_worth in zip(predictions, ages, net_worths):
+		error = abs(net_worth - pred)
+		#print net_worth, pred
+		#print error
+		print type(float(pred))
+		cleaned_data.append((float(age), float(net_worth), float(error)))
+	#print type(cleaned_data[0])
+	cleaned_data = sorted(cleaned_data, key = lambda tup: tup[2])[0:80]
+	pprint.pprint(cleaned_data)
+	#cleaned_data = []
+	return cleaned_data
