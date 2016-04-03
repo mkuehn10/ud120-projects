137aa4e72e9a0cb3cad315ee806eac49013beea5
Michael Kuehn <mkuehn10@gmail.com>
2016-04-03 14:52:07 -0400

    Complete text_learning mini-project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -36,26 +36,35 @@ word_data = []
 ### can iterate your modifications quicker
 temp_counter = 0
 
+from sklearn.feature_extraction.text import CountVectorizer
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+        #temp_counter += 1
+        temp_counter = True
+        #if temp_counter < 200:
+        if temp_counter:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            for word in ["sara", "shackleton", "chris", "germani"]:
+                #print word
+                text = text.replace(word, "")
+                #print text
             ### append the text to word_data
-
+            word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == "chris":
+                from_data.append(1)
+            else:
+                from_data.append(0)
 
             email.close()
 
@@ -65,11 +74,20 @@ from_chris.close()
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+#print from_data
+#print word_data[152]
 
 
 
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(stop_words = 'english')
+X = vectorizer.fit_transform(word_data)
+print len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[34597]
+#print X.toarray()
+from sklearn.feature_extraction.text import TfidfTransformer
+transformer = TfidfTransformer()
+tfidf = transformer.fit_transform(X)
